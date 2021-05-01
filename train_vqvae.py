b8f5d8f80352a71c99dd292c3e53aac94ef0c549
Kushagra Pandey <kpandeyce008@gmail.com>
2021-05-01 20:11:14 +0530

    Test

--- train_vqvae.py
+++ train_vqvae.py
@@ -43,43 +43,49 @@ def train(epoch, loader, model, optimizer, scheduler, device):
             scheduler.step()
         optimizer.step()
 
-        part_mse_sum = recon_loss.item() * img.shape[0]
-        part_mse_n = img.shape[0]
-        comm = {"mse_sum": part_mse_sum, "mse_n": part_mse_n}
-        comm = dist.all_gather(comm)
+        loader.set_postfix_str(
+            f'''
+                epoch: {epoch + 1}; mse: {recon_loss.item():.5f};
+                latent: {latent_loss.item():.3f}; avg mse: {mse_sum / mse_n:.5f};
+                lr: {lr:.5f}
+            '''
+        )
 
-        for part in comm:
-            mse_sum += part["mse_sum"]
-            mse_n += part["mse_n"]
+        # part_mse_sum = recon_loss.item() * img.shape[0]
+        # part_mse_n = img.shape[0]
+        # comm = {"mse_sum": part_mse_sum, "mse_n": part_mse_n}
+        # comm = dist.all_gather(comm)
 
-        if dist.is_primary():
-            lr = optimizer.param_groups[0]["lr"]
+        # for part in comm:
+        #     mse_sum += part["mse_sum"]
+        #     mse_n += part["mse_n"]
 
-            loader.set_description(
-                (
-                    f"epoch: {epoch + 1}; mse: {recon_loss.item():.5f}; "
-                    f"latent: {latent_loss.item():.3f}; avg mse: {mse_sum / mse_n:.5f}; "
-                    f"lr: {lr:.5f}"
-                )
-            )
+        # if dist.is_primary():
+        #     lr = optimizer.param_groups[0]["lr"]
 
-            if i % 100 == 0:
-                model.eval()
+        #     loader.set_postfix_str(f'''
+        #         epoch: {epoch + 1}; mse: {recon_loss.item():.5f};
+        #         latent: {latent_loss.item():.3f}; avg mse: {mse_sum / mse_n:.5f};
+        #         lr: {lr:.5f}
+        #     ''')
 
-                sample = img[:sample_size]
+        #     if i % 100 == 0:
+        #         model.eval()
 
-                with torch.no_grad():
-                    out, _ = model(sample)
+        #         sample = img[:sample_size]
 
-                utils.save_image(
-                    torch.cat([sample, out], 0),
-                    f"sample/{str(epoch + 1).zfill(5)}_{str(i).zfill(5)}.png",
-                    nrow=sample_size,
-                    normalize=True,
-                    range=(-1, 1),
-                )
+        #         with torch.no_grad():
+        #             out, _ = model(sample)
 
-                model.train()
+        #         utils.save_image(
+        #             torch.cat([sample, out], 0),
+        #             f"sample/{str(epoch + 1).zfill(5)}_{str(i).zfill(5)}.png",
+        #             nrow=sample_size,
+        #             normalize=True,
+        #             range=(-1, 1),
+        #         )
+
+        #         model.train()
 
 
 def main(args):
@@ -97,7 +103,7 @@ def main(args):
     )
 
     # dataset = datasets.ImageFolder(args.path, transform=transform)
-    dataset = CUBDataset(args.path, mode='train', transform=transform)
+    dataset = CUBDataset(args.path, transform=transform, mode=args.mode)
     sampler = dist.data_sampler(dataset, shuffle=True, distributed=args.distributed)
     loader = DataLoader(
         dataset, batch_size=128 // args.n_gpu, sampler=sampler, num_workers=2
@@ -145,6 +151,7 @@ if __name__ == "__main__":
     parser.add_argument("--size", type=int, default=256)
     parser.add_argument("--epoch", type=int, default=560)
     parser.add_argument("--lr", type=float, default=3e-4)
+    parser.add_argument("--mode", type=str, default='train')
     parser.add_argument("--sched", type=str)
     parser.add_argument("path", type=str)
 
